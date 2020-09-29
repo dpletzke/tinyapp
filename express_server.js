70dc98e944e120c6ae1a81ddfa5a263f8030d2f1
Dan Pletzke <dpletzke@gmail.com>
2020-09-29 04:54:14 +0000

    url shortening part 2 start

--- express_server.js
+++ express_server.js
@@ -4,8 +4,15 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 app.set("view engine", "ejs");
+
+function generateRandomString() { 
+  var result = '';
+  while (!result) {
+    result = Math.random().toString(36).substring(2, 8);
+    return result;
+  }
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,6 +35,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 
